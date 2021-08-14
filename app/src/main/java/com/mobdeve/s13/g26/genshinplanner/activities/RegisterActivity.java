cfb706da9951ff409e3bd6fe04a7880c88207a94
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-08-14 16:03:35 +0800

    added RegisterActivity

---
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.s13.g26.genshinplanner.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobdeve.s13.g26.genshinplanner.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
