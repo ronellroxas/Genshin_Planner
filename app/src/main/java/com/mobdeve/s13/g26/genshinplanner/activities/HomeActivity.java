b001e852b0b6193a405e08cef196a95768ee1207
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-08-14 19:17:30 +0800

    added path to View Profile

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/HomeActivity.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.mobdeve.s13.g26.genshinplanner.R;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ImageButton btnViewCharacters;
+    private ImageView  imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        this.imageView = findViewById(R.id.imageView);
         this.btnViewCharacters = findViewById(R.id.btn_home_vwchar);
+
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ViewProfileActivity.class);
+
+            startActivity(intent);
+        });
 
         btnViewCharacters.setOnClickListener(v -> {
             Intent intent = new Intent(HomeActivity.this, ListActivity.class);
