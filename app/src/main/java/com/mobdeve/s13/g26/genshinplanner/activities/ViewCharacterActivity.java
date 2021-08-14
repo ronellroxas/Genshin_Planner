b48c1d23f48d05053d7eb53c084ad8a66a44dced
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-08-14 22:05:14 +0800

    added view for view character

---
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/ViewCharacterActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.s13.g26.genshinplanner.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobdeve.s13.g26.genshinplanner.R;
+
+public class ViewCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_character);
+    }
+}
