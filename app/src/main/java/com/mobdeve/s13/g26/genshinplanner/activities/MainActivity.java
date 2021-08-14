db61c7ddd60f31c485784c22b77988e7ed5a6230
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-08-14 17:19:42 +0800

    Added Home activity (with session) and button redirection

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/MainActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/MainActivity.java
@@ -2,15 +2,27 @@ package com.mobdeve.s13.g26.genshinplanner.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.mobdeve.s13.g26.genshinplanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnGoogle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnGoogle =  findViewById(R.id.bnt_home_google);
+
+        btnGoogle.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/RegisterActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/RegisterActivity.java
@@ -2,15 +2,27 @@ package com.mobdeve.s13.g26.genshinplanner.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.mobdeve.s13.g26.genshinplanner.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button btnSaveProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+
+        this.btnSaveProfile = findViewById(R.id.btn_register_saveprofile);
+
+        btnSaveProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+
+            startActivity(intent);
+        });
     }
 }
