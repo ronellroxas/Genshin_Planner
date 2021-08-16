46c642fe8dd8f3e5ccef2d8bdad7d46d4ae5c8b4
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-08-16 17:06:51 +0800

    Added Initial Google Login

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/MainActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/MainActivity.java
@@ -1,28 +1,95 @@
 package com.mobdeve.s13.g26.genshinplanner.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.identity.*;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.mobdeve.s13.g26.genshinplanner.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnGoogle;
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private SignInButton btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.btnGoogle =  findViewById(R.id.btn_main_google);
+        initializeGoogleAuth();
+
+        this.btnGoogle = findViewById(R.id.btn_main_google);
 
         btnGoogle.setOnClickListener(v -> {
+            OpenGoogleTapSignIn();
             Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
 
             startActivity(intent);
         });
+
+
     }
+
+    /**
+     * Initializes google auth stuff
+     */
+    private void initializeGoogleAuth() {
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+
+    }
+
+    /**
+     * Initializes google one tap sign in
+     */
+    private void OpenGoogleTapSignIn() {
+        oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
+                    @Override
+                    public void onSuccess(BeginSignInResult result) {
+                        try {
+
+                            startIntentSenderForResult(
+                                    result.getPendingIntent().getIntentSender(), 1,
+                                    null, 0, 0, 0);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.e("GOOGLE_ERROR:", "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // No saved credentials found. Launch the One Tap sign-up flow, or
+                        // do nothing and continue presenting the signed-out UI.
+                        Log.d("GOOGLE_ERROR", e.getLocalizedMessage());
+                    }
+                });
+    }
+
+
 }
