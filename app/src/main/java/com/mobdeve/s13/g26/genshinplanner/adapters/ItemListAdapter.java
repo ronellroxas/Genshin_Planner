66ba88d5cd151098b0bccc4dd542f379436798a1
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-08-23 17:31:08 +0800

    updated ItemListAdapter

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/adapters/ItemListAdapter.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/adapters/ItemListAdapter.java
@@ -10,12 +10,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mobdeve.s13.g26.genshinplanner.R;
 import com.mobdeve.s13.g26.genshinplanner.models.Item;
 import com.mobdeve.s13.g26.genshinplanner.views.CharacterListViewHolder;
+import com.mobdeve.s13.g26.genshinplanner.views.ItemListViewHolder;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class ItemListAdapter extends RecyclerView.Adapter<CharacterListViewHolder> {
+public class ItemListAdapter extends RecyclerView.Adapter<ItemListViewHolder> {
 
     public static final String KEY_PICTURE = "KEY_PICTURE";
     public static final String KEY_NAME = "KEY_NAME";
@@ -30,10 +31,10 @@ public class ItemListAdapter extends RecyclerView.Adapter<CharacterListViewHolde
     @NonNull
     @NotNull
     @Override
-    public CharacterListViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+    public ItemListViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View itemView = layoutInflater.inflate(R.layout.data_list_character, parent, false);
-        CharacterListViewHolder clViewHolder = new CharacterListViewHolder(itemView);
+        ItemListViewHolder clViewHolder = new ItemListViewHolder(itemView);
 
 
 
@@ -41,7 +42,7 @@ public class ItemListAdapter extends RecyclerView.Adapter<CharacterListViewHolde
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @NotNull CharacterListViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull @NotNull ItemListViewHolder holder, int position) {
         holder.setImageCharacter(itemList.get(position).getItem_img());
     }
 
