185af9d8580ae4be7de65d7a30e5837d1efd5ced
ronellroxas <61377760+ronellroxas@users.noreply.github.com>
2021-08-27 03:19:42 +0800

    Added logout

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/utils/FirebaseUserDBHelper.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/utils/FirebaseUserDBHelper.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -35,5 +36,9 @@ public class FirebaseUserDBHelper {
 
     public DatabaseReference getReference() {
         return userRef;
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
     }
 }
