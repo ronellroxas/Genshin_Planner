4a9a3d29ae5e02419328049e6f82290e343ed910
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-08-27 16:00:44 +0800

    shared preference for View Profile

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/ViewProfileActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/ViewProfileActivity.java
@@ -4,32 +4,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 import com.mobdeve.s13.g26.genshinplanner.R;
 import com.mobdeve.s13.g26.genshinplanner.adapters.CharacterListAdapter;
+import com.mobdeve.s13.g26.genshinplanner.keys.UserKeys;
 import com.mobdeve.s13.g26.genshinplanner.models.Character;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
     private ArrayList<Character> charList;
     private ArrayList<String> userown_list;
 
+    private CircleImageView civ_picture;
+    private TextView tv_name;
+    private TextView tv_uid;
+    private TextView tv_email;
+
     private RecyclerView char_recycler_view;
     private CharacterListAdapter characterListAdapter;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
+        this.sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        this.civ_picture = findViewById(R.id.iv_view_prof_picture);
+        this.tv_name = findViewById(R.id.tv_view_prof_username);
+        this.tv_uid = findViewById(R.id.tv_view_prof_uid);
+        this.tv_email = findViewById(R.id.tv_view_prof_email);
+
         charList = new ArrayList<Character>();
         userown_list = new ArrayList<String>();
         getUserOwnedCharacters();
-        initRecyclerAdapter();
+        initializeDetails();
+        //initRecyclerAdapter();
+    }
+
+    private void initializeDetails(){
+        if(getImageResources(sp.getString(UserKeys.MAIN_KEY.name(),null)) == -1){
+            this.civ_picture.setImageResource(R.drawable.baal);
+        }else{
+            this.civ_picture.setImageResource(getImageResources(sp.getString(UserKeys.MAIN_KEY.name(), null)));
+        }
+        this.tv_name.setText(sp.getString(UserKeys.USERNAME_KEY.name(), null));
+        this.tv_uid.setText(sp.getString(UserKeys.UID_KEY.name(), null));
+        this.tv_email.setText(sp.getString(UserKeys.EMAIL_KEY.name(), null));
+    }
+
+    private int getImageResources(String main) {
+        Field[] fields = R.drawable.class.getDeclaredFields();
+        try {
+            for (int i = 0; i < fields.length; i++) {
+                String filename = fields[i].getName();
+
+                if (filename.contains("characters_") && filename.contains(main.toLowerCase())) //remove filenames with _
+                    return fields[i].getInt(null);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return -1;
     }
 
     private void initRecyclerAdapter() {
