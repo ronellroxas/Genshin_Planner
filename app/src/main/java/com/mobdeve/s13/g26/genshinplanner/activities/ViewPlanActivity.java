5c8af38c69dfb6da7d8af5933c7c8ad421624cb9
simpleuser11 <52992041+BryanTio@users.noreply.github.com>
2021-09-10 16:45:58 +0800

    added ViewPlanActivity backend and  FirebasePlanDBHelper

--- app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/ViewPlanActivity.java
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/activities/ViewPlanActivity.java
@@ -1,19 +1,38 @@
 package com.mobdeve.s13.g26.genshinplanner.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mobdeve.s13.g26.genshinplanner.R;
+import com.mobdeve.s13.g26.genshinplanner.adapters.CreatePlanRoutesAdapter;
+import com.mobdeve.s13.g26.genshinplanner.adapters.ItemListAdapter;
 import com.mobdeve.s13.g26.genshinplanner.keys.PlanKeys;
+import com.mobdeve.s13.g26.genshinplanner.models.Item;
 import com.mobdeve.s13.g26.genshinplanner.models.Plan;
+import com.mobdeve.s13.g26.genshinplanner.utils.FirebasePlanDBHelper;
+import com.squareup.okhttp.Route;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public class ViewPlanActivity extends AppCompatActivity {
+
+    private FirebasePlanDBHelper planDBHelper;
 
     private ImageView iv_user_image;
     private TextView tv_plan_name;
@@ -25,8 +44,13 @@ public class ViewPlanActivity extends AppCompatActivity {
     private RatingBar rb_rating;
 
     private RecyclerView rv_item;
-    private RecyclerView rv_routes;
+    private ItemListAdapter itemListAdapter;
 
+    private RecyclerView rv_routes;
+    private CreatePlanRoutesAdapter planRoutesAdapter;
+
+    private ArrayList<Item> item_list;
+    private ArrayList<String> route_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +65,14 @@ public class ViewPlanActivity extends AppCompatActivity {
         this.rb_rating = findViewById(R.id.rb_plan_view_rating);
 
         this.rv_item = findViewById(R.id.rv_item);
+        this.itemListAdapter = new ItemListAdapter(this.item_list);
+        this.rv_item.setLayoutManager(new  LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        this.rv_item.setAdapter(this.itemListAdapter);
+
         this.rv_routes = findViewById(R.id.rv_routes);
+        this.planRoutesAdapter = new CreatePlanRoutesAdapter(this.route_list);
+        this.rv_routes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        this.rv_routes.setAdapter(this.planRoutesAdapter);
 
         Intent intent = getIntent();
         this.iv_user_image.setImageResource(intent.getIntExtra(PlanKeys.PLAN_IMAGE.name(), 0));
@@ -52,5 +83,28 @@ public class ViewPlanActivity extends AppCompatActivity {
         this.tv_plan_resin.setText(intent.getStringExtra(PlanKeys.PLAN_RESIN_KEY.name()));
         this.rb_rating.setRating(intent.getIntExtra(PlanKeys.PLAN_RATING_KEY.name(), 0));
 
+        planDBHelper = new FirebasePlanDBHelper();
+        Query query = planDBHelper.getReference().limitToFirst(1).orderByChild("id").equalTo(intent.getIntExtra(PlanKeys.PLAN_ID_KEY.name(), 0));
+        query.addListenerForSingleValueEvent(valueEventListener);
     }
+
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+            if(snapshot.exists()){
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Plan curr_plan = dataSnapshot.getValue(Plan.class);
+                    item_list.addAll(curr_plan.getPlan_items());
+                    route_list.addAll(curr_plan.getPlan_route());
+                }
+                itemListAdapter.notifyDataSetChanged();
+                planRoutesAdapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+        }
+    };
 }
---
+++ app/src/main/java/com/mobdeve/s13/g26/genshinplanner/utils/FirebasePlanDBHelper.java
@@ -0,0 +1,39 @@
+package com.mobdeve.s13.g26.genshinplanner.utils;
+
+import android.renderscript.Sampler;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.mobdeve.s13.g26.genshinplanner.models.Plan;
+import com.mobdeve.s13.g26.genshinplanner.models.User;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FirebasePlanDBHelper {
+
+    private DatabaseReference planRef;
+    //private ArrayList<User> users = new ArrayList<>();
+
+    public FirebasePlanDBHelper() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://genshin-planner-323108-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        planRef = database.getReference("plans");
+
+    }
+
+    public void addPlan(Plan plan) {
+        planRef.child(plan.getPlan_id() + "").setValue(plan);
+    }
+
+    public DatabaseReference getReference() {
+        return planRef;
+    }
+}
+
